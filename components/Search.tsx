529b1dfe1380f53feea9ea1b70c584655d7e272a
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-27 12:30:38 +0300

    add chat search input  wrapper component

---
+++ components/Search.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { TUser } from "@/utils/types/type"
+import { getTokenFromCookies } from "@/utils/types/utils"
+import { Dispatch, FormEvent, SetStateAction, useState } from "react"
+
+const Search = ({setUserData}: {setUserData: Dispatch<SetStateAction<TUser | null>>}) => {
+  const [searchKey, setSearchKey] = useState("")
+  const [err, setErr] = useState("")
+
+  async function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const token = getTokenFromCookies()
+    const res = await fetch(`http://localhost:1234/search?search_key=${searchKey}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+        "content-type": "application/json"
+      }
+    })
+    if (!res.ok) {
+      setErr(res.statusText)
+    }
+    const {userResult} = await res.json()
+    // console.log(userResult)
+    setUserData(userResult)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="self-center">
+       <input type="seach" name="search_key" onChange={(e) => setSearchKey(e.target.value)} placeholder="search usernames" className="pl-2 bg-green-900 bg-opacity-80 rounded-lg h-8 w-[200px] text-black" required/>
+       <button className="border-[1px] hover:border-black w-10 rounded-lg h-8" type="submit">👁️</button><br />
+       {err && <span className="text-red-500 text-[12px]">Error: {err}!</span>}
+    </form>
+  )
+}
+
+export default Search
