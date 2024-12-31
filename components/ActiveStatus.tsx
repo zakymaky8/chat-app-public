6854d57d0528852773d2d63dcf13e9754e59db40
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-31 20:43:21 +0300

    edit active offline status

--- components/ActiveStatus.tsx
+++ components/ActiveStatus.tsx
@@ -1,27 +1,10 @@
 "use client"
 
 import { TUser } from "@/utils/types/type"
-// import { Dispatch, SetStateAction } from "react"
-// import { socket } from "./WriteMessage"
-
-// setChatList: Dispatch<SetStateAction<TUser[]>>
 const ActiveStatus = ({user}: {user: TUser}) => {
-    // // const [isActive, setIsActive] = useState(user.isActive)
-    // useEffect(() => {
-    //     socket.on("get offline", users => {
-    //             setChatList(users)
-    //     })
-    //     socket.on("get online", users => {
-    //         setChatList(users)
-    //     })
-    //     return () => {
-    //         socket.off("get offline")
-    //         socket.off("get online")
-    //     }
-    // }, [])
   return (
     <>
-      {user.isActive && <span className='text-yellow-300 text-[11px] w-fit rounded-[50%] pl-2 pr-2 p-[6px] ml-5 bg-[green]'>active</span> }
+      {user.isActive ? <span className='text-yellow-300 text-[11px] w-fit rounded-[50%] pl-2 pr-2 p-[6px] ml-5 bg-[green]'>active</span> : <span className="text-10px text-black opacity-70">Offline</span> }
     </>
   )
 }
