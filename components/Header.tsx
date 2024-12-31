91411c10601494c5aee72c1ae797cc042e410abd
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2024-12-31 20:42:28 +0300

    edit header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,13 @@
 "use client"
 import Link from "next/link"
-import { useRouter } from "next/navigation"
 
 const Header = () => {
-  const router = useRouter()
   return (
     <header className="bg-[#0e2917] p-6 pt-10 b-10 flex justify-between items-center flex-wrap gap-5">
         <h1 className="text-4xl">Chatter</h1>
         <div className="flex gap-10">
           <Link href="/" className="hover:opacity-70">Home</Link>
           <Link href="/chats#chats" className="hover:opacity-70">Chats</Link>
-          <button onClick={() => {
-              document.cookie = `token=${null}; path=/; secure`
-              router.push("/login")
-          }}>Log out</button>
           <Link href="/search" className="hover:opacity-70">Search User to Chat!</Link>
         </div>
 
