ed9f9df45357620644895de97fea1bf88638fcf0
Gizachew Meku <zekaryasmekuaninit@gmail.com>
2025-01-01 22:32:32 +0300

    modify conversations list

--- components/Conversations.tsx
+++ components/Conversations.tsx
@@ -13,10 +13,12 @@ export type TData = {currentUser: TUser | null, targetedUser: TUser | null, chat
 const Conversations = ({chatId}: {chatId: string}) => {
 
     const [data, setData] = useState<TData>({chatCollections: [], currentUser: null, targetedUser: null})
-
+    const [isLoading, setIsLoading] = useState(true)
     const router = useRouter()
+
     useEffect(() => {
         async function getConversations() {
+          // await new Promise(resolve => setTimeout(resolve, 9000))
             const token = getTokenFromCookies()
             const res = await fetch(`http://localhost:1234/chats?target=${chatId}`, {
                 headers: {
@@ -25,30 +27,33 @@ const Conversations = ({chatId}: {chatId: string}) => {
               },
             });
             if (!res.ok) {
+              setIsLoading(false)
               socket.emit("go offline", data?.currentUser?._id)
               document.cookie = `token=${null}; path=/; secure`
               router.replace("/login")
             }
+            setIsLoading(false)
             setData( await res.json())
         }
         getConversations()
     }, [data])
   return (
-    <div className="relative flex flex-col min-h-[502px] h-max justify-between">
-      <div className="self-center text-[12px] p-[2px] sticky top-0  pl-4 pr-4 mt-[2px] bg-[rgb(6,39,12)] gap-4 flex justify-between w-full">
-        <h4 className="text-center text-white text-opacity-70">You: <strong className="text-yellow-500 italic">{data.currentUser?.username}</strong></h4>
-        <h4 className="text-center text-white text-opacity-70">With: <strong className="text-yellow-500 italic">{data.targetedUser?.username}</strong></h4>
-      </div>
-      <ul className="flex flex-col gap-3 p-3 mb-20">
+    <div className={`flex ${data.currentUser?.preferences.theme === "light" ? "bg-yellow-950" : "bg-black"} flex-col grow overflow-auto border-t-[4px] border-opacity-55 border-black`} style={{scrollbarWidth: "none"}}>
+      <div className="relative flex flex-col min-h-[502px] h-max justify-between">
+        <div className="self-center text-[12px] p-[2px] sticky top-0  pl-4 pr-4 mt-[2px] bg-[rgb(6,39,12)] gap-4 flex justify-between w-full">
+          <h4 className="text-center text-white text-opacity-70">You: <strong className="text-yellow-500 italic">{data.currentUser?.username}</strong></h4>
+          <h4 className="text-center text-white text-opacity-70">With: <strong className="text-yellow-500 italic">{data.targetedUser?.username}</strong></h4>
+        </div>
+        <ul className="flex flex-col gap-3 p-3 mb-20">
 
-        {
-          data.chatCollections.length !==0 ?
-          data.chatCollections.map(chat => {
-            return <ChatMsg setData={setData} key={chat._id} chatdata={chat} current = {data.currentUser} target={data.targetedUser} />
-          }) : <li className='text-yellow-600 text-center'>No chat history!</li>
-        }
-      </ul>
-      <WriteMessage data={data} setData={setData} targetedUser={data.targetedUser} currentUser={data.currentUser} />
+          {isLoading  ? <span className="self-center spinner"></span> : data.chatCollections.length !==0 ?
+            data.chatCollections.map(chat => {
+              return <ChatMsg setData={setData} key={chat._id} chatdata={chat} current = {data.currentUser} target={data.targetedUser} />
+            }) : <li className='text-yellow-600 text-center'>No chat history!</li>
+          }
+        </ul>
+        <WriteMessage data={data} setData={setData} targetedUser={data.targetedUser} currentUser={data.currentUser} />
+      </div>
     </div>
   )
 }
